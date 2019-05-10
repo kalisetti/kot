bf5c7228beda13a86940f5cc7ef33a0de10952d1
SivaSankar K <sivasankar.k2003@gmail.com>
2019-05-10 18:28:20 +0600

    updates 2019/05/10 6:28PM

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express 		= require('express');
 var app 			= express();
 var http 			= require('http').Server(app);
 var io 				= require('socket.io')(http);
+var fs 				= require('fs');
 var session 		= require('express-session');
 var bodyParser 		= require('body-parser'); // For parsing JSON and url-encoded data
 var multer			= require('multer'); // For parsing multipart/form data
 var upload 			= multer();
 var path 			= require('path');
-var mysql 			= require('mysql');
+var db 				= require('./config/db.js');
 var cookieParser 	= require('cookie-parser'); // cookie-parser is a middleware which parses cookies attached to the client request object.
+var conf 			= get_conf();
 
 // Notifications
 const notifier 		= require('node-notifier');
@@ -20,13 +22,6 @@ app.use('/things', things)
 // Set templating engine
 app.set('view engine', 'pug');
 app.set('views', './views');
-
-var con = mysql.createConnection({
-	host : 'localhost',
-	user : 'kot',
-	password : 'kot',
-	database: 'kot'
-});
 
 app.use(session({
 	secret: 'secret',
@@ -49,118 +44,44 @@ app.use(express.static('assets'));
 // Cookies
 app.use(cookieParser());
 
-// Pug page
-app.get('/first_template', function(req, res){
-	res.render('first_view');
-});
-app.get('/dynamic_view',function(req, res){
-	res.render('dynamic', {
-		name: "TutorialsPoint",
-		url: "http://www.tutorialspoint.com",
-		user: {name: "siva@bt.bt", age: "35"}
-	});
-});
-app.get('/components', function(req, res){
-	res.render('content');
-});
-app.get('/form', function(req, res){
-	res.render('form');
-});
-// Pug End
+//Moved routes to another file
+var controllers = require('./controllers');
+controllers.set(app);
 
-app.get('/', function(req, res){
-	console.log("Cookies: ", req.cookies);
-	// Setting a cookie
-	res.cookie('name', 'express'); //Sets name=express
-	res.cookie('silly', 'feeling silly', {maxAge: 60000}); //This cookies expires after 60000ms from the time it is set
-	res.clearCookie('foo'); //Deleting Existing Cookies 
-	
-	if (req.session.loggedin){
-		res.send('Welcome back, '+ req.session.username + '!')
-	} else {
-		res.sendFile(path.join(__dirname, '/login.html'));
-	} 
-});
-
-app.post('/', function(req, res){
-	console.log(req.body);
-	res.send("received your request!..");
-});
-
-app.get('/auth', function(req, res){
-	res.send('Unauthorized access');
-	res.end();
-});
-
-app.io = io;
-
-app.post('/auth', function(req, res){
-	var username = req.body.username;
-	var password = req.body.password;
-	
-	if(username && password){
-		con.query('SELECT * FROM tabuser WHERE username = ? AND password = ?', [username, password], function(error, results, fields){
-			if (results.length > 0){
-				req.session.loggedin = true;
-				req.session.username = username;
-				res.redirect('/home');
-			} else {
-				res.send('Incorrect Username and/or Password!');
-				req.app.io.emit('tx', "hahahaha");
-			}
-			res.end();
-		});
-	} else {
-		res.send('Please enter Username and Password!');
-		res.end();
-	}
-});
-
-app.get('/logout', function(req, res){
-	req.session.loggedin = false;
-	res.redirect('/');
-	res.end();
-});
-
-app.get('/home', function(req, res){
-	if (req.session.loggedin){
-		res.send('Welcome back, '+ req.session.username + '!')
-	} else {
-		//res.send('Please login to view this page!');
-		res.redirect('/'); //shiv added
-	}
-	res.end();
-});
-
-/*
-io.on('connection', function(socket){
-	console.log('A user connected...ssss');
-	
-	socket.on('disconnect', function(){
-		console.log('A user disconnected...zzz');
-	});
-});
-*/
-
-//Other routes here
-//Important - This should be placed after all your routes, as Express matches
-//    routes from start to end of the app.js/index.js file, including the 
-//	  external routers you required.
-app.get('*', function(req, res){
-	res.send('Sorry, this is an invalid URL.');
-});
-
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(conf.app_port, function(){
+	console.log('listening on *:',conf.app_port);
 	notifier.notify({
 		'title': 'KITCHEN ORDER TICKET SYSTEM',
-		'message': 'Listening on *:3000',
+		'message': 'Listening on *:'+conf.app_port,
 		'icon': path.join(__dirname, 'assets/images/cupicon.gif'),
 		sound: true, // Only Notification Center or Windows Toasters
 		wait: true // Wait with callback, until user action is taken against notification
 	}, function(err, res){
 		// Response is response from notification
-		console.log('Notifier response ');
 		console.log(res);
 	});
 });
+
+function get_conf() {
+        // defaults
+        var conf = {
+                redis_async_broker_port: 12311,
+                socketio_port: 3000
+        };
+
+        var read_config = function(path) {
+                if(fs.existsSync(path)){
+                        var app_config = JSON.parse(fs.readFileSync(path));
+                        for (var key in app_config) {
+                                if (app_config[key]) {
+                                        conf[key] = app_config[key];
+                                }
+                        }
+                }
+        }
+
+        // get ports from bench/config.json
+        read_config('./config/config.json');
+
+        return conf;
+}
--- config/db.js
+++ config/db.js
@@ -1,1 +1,32 @@
 //Database connection and configuration
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+	connectionLimit: 100, //important
+	host 	: 'localhost',
+	user 	: 'kot',
+	password: 'kot',
+	database: 'kot',
+	debug 	: false
+});
+
+/*
+module.exports = {
+	sql: function(query){
+		pool.query(query, function(err,rows){
+			if(err){
+				console.log('Error occured',err);
+				//return res.json({"error": true, 'message': 'Error occured '+err, 'code': err.code});
+				return {"error": true, 'message': 'Error occured '+err, 'code': err.code}
+			}
+			
+			//connection will be released as well.
+			console.log('Your data',rows);
+			//res.json(rows);
+			return (rows);
+		});
+	}
+}
+*/
+
+module.exports = pool;
